9d3e46218425217b13e8e4eb13b5f4b226ea374e
hkodapaka <Hari.kodapaka@mccain.ca>
2021-11-09 11:36:23 -0500

    react scrollspy implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,35 +2,27 @@ import './Header.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { scroller } from 'react-scroll';
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-scroll'
 
 function Header(props) {
     const { title, links, varient } = props;
-    const [activeLink, setactiveLink] = useState('intro');
-    function scrollTo(scrollTo) {
-        setactiveLink(scrollTo);
-        scroller.scrollTo(scrollTo, {
-            activeLink: "active-link",
-            smooth: true,
-            spyOn: true,
-            duration: 400,
-        });
-    }
+
     return (
-        <Navbar bg={varient} variant={varient} expand="md" fixed="top">
+        <Navbar bg={varient} variant={varient} expand="md" fixed="top" id="header-navbar">
             <Container>
                 <Navbar.Brand href="#">{title}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
                         {links.map((link, i) =>
-                            <Nav.Link key={i} href="#" active={activeLink === link.fields.scrollTo ? true : false}
-                                onClick={() => { scrollTo(link.fields.scrollTo); }}>
+                            <Link activeClass="active-class" className="nav-link" 
+                                to={link.fields.scrollTo} spy={true} smooth={true} 
+                                duration={500} key={i} offset={-50} href="void(0)"
+                            >
                                 {link.fields.name}
-                            </Nav.Link>
+                            </Link>
                         )}
                     </Nav>
                 </Navbar.Collapse>
