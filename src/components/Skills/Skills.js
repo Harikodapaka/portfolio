3222ad358324af9b5c271fa5dfaba23b1594666a
hkodapaka <Hari.kodapaka@mccain.ca>
2021-11-12 10:51:48 -0500

    skills responsive issue fix

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -18,14 +18,16 @@ function Skills(props) {
             <Container fluid="md">
                 <h1 className="text-white"> {title} </h1>
                 {skill_catrgories &&
-                    <Carousel className="mt-3 card" variant="dark" controls={false} indicators={true} interval={2000} pause={false}>
+                    <Carousel className="mt-3 card carousel-fade" variant="dark" controls={false} indicators={true} interval={2000} pause={false}>
                         {skill_catrgories.map((skillType, i) => (
-                            <Carousel.Item key={i} className="mt-3 p-5" style={{ minHeight: "25em" }}>
+                            <Carousel.Item key={i} className="mt-3 p-3" style={{ minHeight: "25em" }}>
+                                <div className="display-6 text-center">{skillType.fields.name}</div>
+                                <hr/>
                                 <Row className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 align-items-center">
                                     {skillType.fields?.skills?.map((skill, i) => (
                                         <Col key={i}>
                                             <div className="d-flex align-items-center">
-                                                <span className="col-3 col-md-5 text-center icon">
+                                                <span className="col-3 col-md-4 text-center icon">
                                                     {GetImage(skill?.fields?.img, skill?.fields?.iconColor)}
                                                 </span>
                                                 <ProgressBar className="col-9 col-md-7" now={skill?.fields?.percent} label={skill?.fields?.name} variant={skill?.fields?.img} />
@@ -33,9 +35,6 @@ function Skills(props) {
                                         </Col>
                                     ))}
                                 </Row>
-                                <Carousel.Caption>
-                                    <h3>{skillType.fields.name}</h3>
-                                </Carousel.Caption>
                             </Carousel.Item>
                         ))}
                     </Carousel>
